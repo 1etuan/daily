926f829bff05858c6d73f4ba139a95ca4fac7020
letuan <letuan2372000@gmail.com>
2022-09-13 23:48:01 +0700

    day2

---
+++ 967. Numbers With Same Consecutive Differences.js
@@ -0,0 +1,36 @@
+// https://leetcode.com/problems/numbers-with-same-consecutive-differences/
+
+var numsSameConsecDiff = function (n, k) {
+
+    let queue = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let level = 1;
+
+    while (queue.length) {
+        let queueSize = queue.length;
+
+        level = level + 1;
+
+        while (queueSize > 0) {
+            let front = queue.shift();
+            queueSize--
+
+            let lastNum = front % 10;
+
+            if (lastNum + k <= 9) {
+                queue.push(front * 10 + (lastNum + k));
+            }
+
+            if (lastNum - k >= 0) {
+                queue.push(front * 10 + (lastNum - k));
+            }
+        }
+
+        if (level === n) {
+            break;
+        }
+    }
+    console.log(queue)
+    return queue;
+};
+
+numsSameConsecDiff(3, 7)
